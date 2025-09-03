e8af5bd208c972551cab628a85c050dee5dce8d0
minalee <mina.lee066@gmail.com>
2025-09-03 09:52:52 +0100

    Added .env file with url and added to gitignore

--- src/hooks/tanstack/useLungCarcinomaAssociatedTargets.ts
+++ src/hooks/tanstack/useLungCarcinomaAssociatedTargets.ts
@@ -24,9 +24,7 @@ export const GET_DISEASE_ASSOCIATED_TARGETS = gql`
 	}
 `;
 
-const API_URL = 'https://api.platform.opentargets.org/api/v4/graphql';
-
-const graphqlClient = new GraphQLClient(API_URL);
+const graphqlClient = new GraphQLClient(import.meta.env.VITE_API_URL);
 
 export default function useLungCarcinomaAssociatedTargets() {
 	return useQuery<LungCarcinomaAssociatedTargetsQuery, Error>({
